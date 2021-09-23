9706d38f66829deacb231daec9660fd192f245f9
Aurélien GIRAUD <giraudan@skysoft-atm.com>
2021-09-23 11:42:56 +0900

    few minor changes

--- webapp/main.py
+++ webapp/main.py
@@ -8,7 +8,7 @@ from webapp import WebApp
 if __name__ == '__main__':
     cherrypy.config.update({
         'server.socket_host': '0.0.0.0',
-        'server.socket_port': 8099})
+    })
     web_app = WebApp(config)
     conf = {
         '/': {
